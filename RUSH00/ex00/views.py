26380d49e625e58c3750a624162bf710188c972f
Kai Carver <kaicarver@gmail.com>
2019-01-13 15:09:01 +0100

    most of the random event during move

--- RUSH00/ex00/views.py
+++ RUSH00/ex00/views.py
@@ -26,41 +26,53 @@ def displayObject(o):
 	#print(str(o.__class__) + ": " + str(o.__dict__))
 	print(o.position)
 
-def worldmap(request):
-	print("hey new game?")
-	move = request.GET.get('move', '')
-	settings = getInfo.moviemon()
-	settings = settings.dump()
-	print("settings.position")
-	print(settings.position)
-	displayObject(settings)
+def do_move(settings, move):
+	did_move = False
 	width = settings.grid_size['width']
 	height = settings.grid_size['height']
 	position = settings.position
 	if (move=='left'):
 		if position['x'] > 0:
 			settings.position['x'] = position['x'] - 1
-			settings.saveTMP()
+			did_move = True
 		return(redirect("/worldmap"))
 	if (move=='right'):
 		if position['x'] < width - 1:
 			settings.position['x'] = position['x'] + 1
-			settings.saveTMP()
+			did_move = True
 		return(redirect("/worldmap"))
 	if (move=='up'):
 		if position['y'] > 0:
 			settings.position['y'] = position['y'] - 1
-			settings.saveTMP()
-		return(redirect("/worldmap"))
+			did_move = True
 	if (move=='down'):
 		if position['y'] < height - 1:
 			settings.position['y'] = position['y'] + 1
-			settings.saveTMP()
-		return(redirect("/worldmap"))
+			did_move = True
+	return did_move
+
+def random_move_event(settings):
 	from random import randint
-	print(randint(0, 1))
-	settings.saveTMP()
-	return render(request, "ex00/worldmap.html", { 'grid':make_grid(width, height, position), 'found': 0 })
+	rand = randint(0, 2)
+	if rand == 1:
+		settings.nombreMovieballs += 1
+	elif rand == 2:
+		junk = 0 # TODO: found a Moviemon
+	return rand
+
+def worldmap(request):
+	move = request.GET.get('move', '')
+	settings = getInfo.moviemon()
+	settings = settings.dump()
+	if do_move(settings, move):
+		from random import randint
+		settings.found = random_move_event(settings)
+		settings.saveTMP()
+		return(redirect("/worldmap"))
+	width = settings.grid_size['width']
+	height = settings.grid_size['height']
+	position = settings.position
+	return render(request, "ex00/worldmap.html", { 'grid':make_grid(width, height, position), 'found': settings.found, 'numballs': settings.nombreMovieballs })
 
 def battle(request, id):
 	print("battle!", id)
