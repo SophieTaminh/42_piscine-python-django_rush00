b3a65ac3dfbcac464bf2feddfe547ddfddb5a9c4
Kai Carver <kaicarver@gmail.com>
2019-01-13 08:08:31 +0100

    remove virtual env dir

--- django_venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/cprouveu/Desktop/PISCINE_PYTHON_DJANGO_RENDU/RUSH00/django_venv/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- django_venv/lib/python3.7/__future__.py
+++
@@ -1,1 +0,0 @@
-/Users/cprouveu/.brew/Cellar/python/3.7.1/Frameworks/Python.framework/Versions/3.7/lib/python3.7/__future__.py
