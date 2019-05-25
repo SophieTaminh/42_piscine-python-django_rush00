ca58118e54b01214ecb9ab42b67778cdacd82d70
Kai Carver <kaicarver@gmail.com>
2019-05-25 15:59:14 +0200

    derniere version Django; script ameliore

--- my_script.sh
+++ my_script.sh
@@ -1,9 +1,9 @@
 pip3 install virtualenv
 virtualenv django_venv; source django_venv/bin/activate
-pip3 --version
 pip3 install -r requirements.txt
-
-sh
+cd RUSH00
+echo To start Django, type: ./manage.py runserver
+PS1='(django_venv) \w> ' sh
 
 # django-admin.py startproject RUSH00
 # cd RUSH00
